e6946116aacf2902012a1a9ba6c24a3b4af207a6
atandon2703 <139319341+atandon2703@users.noreply.github.com>
2023-07-12 12:58:45 +0530

    Final Update DevClass.java (#2)

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,7 +1,7 @@
 public class DevClass {
 
     public static void main(String[] args) {
-        System.out.println("Dev Class Forked Repo");
-        //Changes done in Dev Class Forked Repo
+        System.out.println("Dev Class Forked-Repo");
+        //Changes done in Dev Class Forked-Repo
     }
 }

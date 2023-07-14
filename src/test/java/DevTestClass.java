02f86831f3dc3eb27cce9ba88f4a0c481aaf6230
ArpitTandon <techyarpittandon@gmail.com>
2023-07-14 09:57:28 +0530

    Added print statement to Test Class

--- src/test/java/DevTestClass.java
+++ src/test/java/DevTestClass.java
@@ -3,5 +3,6 @@ public class DevTestClass {
     public static void main(String[] args) {
 
         System.out.println("Dev Test Class");
+        System.out.println("Dev Test Class 789");
     }
 }
